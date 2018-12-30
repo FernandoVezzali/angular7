2ef9024a8c25405ee1e18ee440ded65bedf98067
Fernando <fernandovezzali@gmail.com>
2018-12-30 20:12:32 -0200

    adding text dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  list: Object[] = [];
+
+  constructor() {
+
+    var multipleChoises: string[][] = [];
+    multipleChoises.push(['1', 'Excelente']);
+    multipleChoises.push(['2', 'Muito Bom']);
+    multipleChoises.push(['3', 'Bom']);
+    multipleChoises.push(['4', 'Regular']);
+    multipleChoises.push(['5', 'Ruim']);
+
+    var yesNo: string[][] = [];
+    yesNo.push(['1', 'Sim']);
+    yesNo.push(['2', 'Não']);
+
+    this.addItem('Nossa comunicação:', 'Como qualificaria a comunicação e a informação no website antes do evento?', multipleChoises);
+    this.addItem('Localização:', 'Como qualificaria o local do evento?', multipleChoises);
+    this.addItem('Credenciamento:', 'Como qualificaria a área de credenciamento?', multipleChoises);
+    this.addItem(undefined, 'Voltaria a participar em um evento com esta temática?', yesNo);
+  }
+
+  addItem(title: string, subTitle: string, choises: string[][]) {
+
+    this.list.push({
+      'title': title,
+      'subTitle': subTitle,
+      'choises': choises
+    });
+
+    /*
+    (this._randomIndex(), 0, {
+      'letter': this._randomLetter(),
+      'number': this._randomNumber(),
+      'generation': this.generation,
+      'value': ''
+    });
+    */
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
-//import 'src/polyfills';
-
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DemoMaterialModule } from 'src/material-module';
+import { MaterialModule } from 'src/material-module';
 
 @NgModule({
 
@@ -22,46 +17,11 @@ import { DemoMaterialModule } from 'src/material-module';
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    DemoMaterialModule,
+    MaterialModule,
     MatNativeDateModule,
     ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-
-  list: Object[] = [];
-
-  constructor() {
-
-    var listOfLists: string[][];
-    listOfLists.push(['1', 'Excelente']);
-    listOfLists.push(['2', 'Muito Bom']);
-    listOfLists.push(['3', 'Bom']);
-    listOfLists.push(['4', 'Regular']);
-    listOfLists.push(['5', 'Ruim']);
-
-    this.addItem('a', 'b', listOfLists);
-    this.addItem('c', 'd', listOfLists);
-
-  }
-
-  addItem(title: string, subTitle: string, choises: string[][]) {
-
-    this.list.push({
-      'title': title,
-      'subTitle': subTitle,
-      'choises': choises
-    });
-
-    /*
-    (this._randomIndex(), 0, {
-      'letter': this._randomLetter(),
-      'number': this._randomNumber(),
-      'generation': this.generation,
-      'value': ''
-    });
-    */
-  }
-}
+export class AppModule {}
