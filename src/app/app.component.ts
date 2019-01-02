da9cf7f1fa9de5353cb3edb22e2082069b6a5d1e
unknown <fernandovezzali@gmail.com>
2019-01-02 18:06:09 -0200

    linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,32 @@ export class AppComponent {
 
   constructor() {
 
-    var multipleChoises: string[][] = [];
+    const multipleChoises: string[][] = [];
     multipleChoises.push(['1', 'Excelente']);
     multipleChoises.push(['2', 'Muito Bom']);
     multipleChoises.push(['3', 'Bom']);
     multipleChoises.push(['4', 'Regular']);
     multipleChoises.push(['5', 'Ruim']);
 
-    var yesNo: string[][] = [];
+    const yesNo: string[][] = [];
     yesNo.push(['1', 'Sim']);
     yesNo.push(['2', 'Não']);
 
-    const q1: Question = { id: 1, title: 'Nossa comunicação:', subTitle: 'Como qualificaria a comunicação e a informação no website antes do evento?', choises: multipleChoises };
-    const q2: Question = { id: 2, title: 'Localização:', subTitle: 'Como qualificaria o local do evento?', choises: multipleChoises };
-    const q3: Question = { id: 3, title: 'Credenciamento:', subTitle: 'Como qualificaria a área de credenciamento?', choises: multipleChoises };
-    const q4: Question = { id: 4, title: 'Nossa comunicação:', subTitle: 'Como qualificaria a comunicação e a informação no website antes do evento?', choises: yesNo };
+    const q1: Question = { id: 1, title: 'Nossa comunicação:',
+    subTitle: 'Como qualificaria a comunicação e a informação no website antes do evento?',
+    choises: multipleChoises };
+
+    const q2: Question = { id: 2, title: 'Localização:',
+    subTitle: 'Como qualificaria o local do evento?',
+    choises: multipleChoises };
+
+    const q3: Question = { id: 3, title: 'Credenciamento:',
+    subTitle: 'Como qualificaria a área de credenciamento?',
+    choises: multipleChoises };
+
+    const q4: Question = { id: 4, title: 'Nossa comunicação:',
+    subTitle: 'Como qualificaria a comunicação e a informação no website antes do evento?',
+    choises: yesNo };
 
     this.list.push(q1, q2, q3, q4);
   }
